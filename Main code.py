2f7ad5f2832b983c88c34d2680f45a9577500963
Sheep-IK-LT <yik418@naver.com>
2023-11-14 22:15:39 +0900

    plate 메인 병합, Plate 객체 생성

--- Main code.py
+++ Main code.py
@@ -1,4 +1,28 @@
 from pico2d import *
+import random
+
+class plate:
+    #__cnt = 0   #멤버 변수
+    flying_image = None
+    def __init__(self, x, y, cnt, flying_type):
+        if plate.flying_image == None:
+            plate.flying_image = load_image('Flying Disk.png')
+        print('비행접시 슈우웅')
+        self.flying_type = random.randint(1,2)
+
+    def flying(self, x, y):
+        if self.flying_type == 1:
+            self.x, self.y = 0, 300  #캔버스
+        else:
+            self.x, self.y = 800, 300 #캔버스
+
+    def draw(self):
+        self.flying_image.draw(self.x, self.y)
+
+    def __del__(self):
+        print("객체 소멸")
+
+
 
 #화면 크기 설정
 
@@ -42,14 +66,54 @@ while start:
     delay(0.1)
 
 while mainpage:
+    global plate_gather
+    global head
+
     print('running')
     clear_canvas()
     grass_base.draw(CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
 
     #원판 날아오기
+
+    Plate = plate(0, 400, 1, 1)
+
+    # plate_gather = [plate() for i in range(10)]
+
     #사격 코드
     #사격 충돌 체크
 
     update_canvas()
     delay(0.05)
+
+
+
+def plate_type1(x, y):  #왼쪽에서 오른쪽으로 이동
+    global head
+
+
+
+    while head <= 160:
+        if head <= 80:
+            x += 5
+            y += 2
+        elif head > 80:
+            x += 5
+            y -= 2
+
+        head += 1
+
+
+def plate_type2(x, y):  #오른쪽에서 왼쪽으로 이동
+    global head
+
+
+    while head <= 160:
+        if head <= 80:
+            x -= 5
+            y += 2
+        elif head > 80:
+            x -= 5
+            y -= 2
+
+        head += 1
 
--- plate.py
+++ plate.py
@@ -1,26 +1,6 @@
 from pico2d import *
 import random
 
-class plate:
-    #__cnt = 0   #멤버 변수
-    flying_image = None
-    def __init__(self, x, y, cnt):
-        if plate.flying_image == None:
-            plate.flying_image = load_image('Flying Disk.png')
-        print('비행접시 슈우웅')
-        self.flying_type = random.randint(1,2)
-
-    def flying(self, x, y):
-        if self.flying_type == 1:
-            self.x, self.y = 0, 300  #캔버스
-        else
-            self.x, self.y = 800, 300 #캔버스
-
-    def draw(self):
-        self.flying_image.draw(self.x, self.y)
-
-    def __del__(self):
-        print("객체 소멸")
 
 
 Plate_Number = 20
@@ -28,7 +8,11 @@ Plate_count = 1
 
 
 while Plate_count < Plate_Number:
-    plate(__cnt=Plate_count)
+    plate(cnt=Plate_count)
     Plate_count += 1
+
+head = 0
+
+
 
 
